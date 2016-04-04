e2507323b4484d3987944f7ee129a63ad4073ffa
Matt <matt24ray@gmail.com>
2016-04-03 19:45:05 -0500

    33Rails

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -35,5 +35,35 @@ end
 
 
   def edit
+    @post = Post.find(params[:id])
   end
+
+  def destroy
+     @post = Post.find(params[:id])
+
+ # #8
+     if @post.destroy
+       flash[:notice] = "\"#{@post.title}\" was deleted successfully."
+       redirect_to posts_path
+     else
+       flash.now[:alert] = "There was an error deleting the post."
+       render :show
+     end
+   end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.title = params[:post][:title]
+    @post.body = params[:post][:body]
+
+    if @post.save
+      flash[:notice] = "Post was updated."
+      redirect_to @post
+    else
+      flash.now[:alert] = "There was an error saving the post. Please try again."
+      render :edit
+    end
+  end
+
+  # the real end
 end
