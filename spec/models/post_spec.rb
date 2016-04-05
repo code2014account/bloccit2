494b9c901bcabd186e96645bf5965ed588eb5af2
Matt <matt24ray@gmail.com>
2016-04-04 23:23:21 -0500

    35Rails

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -8,12 +8,19 @@ let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: Random
 
 it { is_expected.to belong_to(:topic) }
 
+  it { is_expected.to validate_presence_of(:title) }
+  it { is_expected.to validate_presence_of(:body) }
+  it { is_expected.to validate_presence_of(:topic) }
+# 2
+  it { is_expected.to validate_length_of(:title).is_at_least(5) }
+  it { is_expected.to validate_length_of(:body).is_at_least(20) }
+
   describe "attributes" do
-# #2
+
     it "responds to title" do
       expect(post).to respond_to(:title)
     end
-# #3
+
     it "responds to body" do
       expect(post).to respond_to(:body)
     end
