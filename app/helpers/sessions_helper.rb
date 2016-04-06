ee78203e9907ca7c3d0df02c8880330079a559e7
Matt <matt24ray@gmail.com>
2016-04-05 19:16:21 -0500

    38Rails

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,15 @@
+module SessionsHelper
+  def create_session(user)
+    session[:user_id] = user.id
+  end
+
+# #5
+  def destroy_session(user)
+    session[:user_id] = nil
+  end
+
+# #6
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create]
 
+  resources :sessions, only: [:new, :create, :destroy]
+
 
   get 'about' => 'welcome#about'
 
